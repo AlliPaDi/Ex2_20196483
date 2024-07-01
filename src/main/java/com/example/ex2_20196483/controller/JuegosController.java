04f4865c7f770813838d79a7d61cb035828893f6
Allison <a20196483@pucp.edu.pe>
2024-07-01 14:28:24 -0500

    avance

--- src/main/java/com/example/ex2_20196483/controller/JuegosController.java
+++ src/main/java/com/example/ex2_20196483/controller/JuegosController.java
@@ -2,11 +2,15 @@ package com.example.ex2_20196483.controller;
 
 import com.example.ex2_20196483.entity.Juegos;
 import com.example.ex2_20196483.repository.JuegosRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
+
 @EnableWebSecurity
 
 @RestController
@@ -21,6 +25,16 @@ public class JuegosController {
     @GetMapping("api/orders/juegos")
     public List<Juegos> listarJuegos(){
         return juegosRepository.findAll();
+    }
+
+    @PostMapping(value="/api/orders/juegos/crear")
+    public ResponseEntity<HashMap<String, Object>> crearJuego(
+     @RequestBody Juegos juego ){
+        HashMap<String, Object> responseMap = new HashMap<>();
+
+        juegosRepository.save(juego);
+        responseMap.put("result", "success");
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseMap);
     }
 
 
